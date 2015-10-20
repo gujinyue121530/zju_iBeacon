0c3e3ad7d6d315d4feb70d9a9f8af4e81f5729a1
gujinyue1010 <361143552@qq.com>
2015-10-20 22:28:24 +0800

    修改

---
+++ 微博/xuyoutian.h
@@ -0,0 +1,13 @@
+//
+//  xuyoutian.h
+//  微博
+//
+//  Created by 123 on 15/10/20.
+//  Copyright © 2015年 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface xuyoutian : UIView
+
+@end
---
+++ 微博/xuyoutian.m
@@ -0,0 +1,21 @@
+//
+//  xuyoutian.m
+//  微博
+//
+//  Created by 123 on 15/10/20.
+//  Copyright © 2015年 123. All rights reserved.
+//
+
+#import "xuyoutian.h"
+
+@implementation xuyoutian
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
