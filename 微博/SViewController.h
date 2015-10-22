de564c10149c8a888c7df63313d3738743e0edd4
chenyufeng <503434355@qq.com>
2015-10-22 20:09:39 +0800

    chenyufeng

---
+++ 微博/SViewController.h
@@ -0,0 +1,13 @@
+//
+//  SViewController.h
+//  微博
+//
+//  Created by chenyufeng on 15/10/22.
+//  Copyright © 2015年 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SViewController : UIViewController
+
+@end
---
+++ 微博/SViewController.m
@@ -0,0 +1,37 @@
+//
+//  SViewController.m
+//  微博
+//
+//  Created by chenyufeng on 15/10/22.
+//  Copyright © 2015年 123. All rights reserved.
+//
+
+#import "SViewController.h"
+
+@interface SViewController ()
+
+@end
+
+@implementation SViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
